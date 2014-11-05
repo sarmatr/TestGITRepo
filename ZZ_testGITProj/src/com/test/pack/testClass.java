1cff956803a259279122f747b0120a7633eb273e
sarmat <sarmat@192.168.2.2>
2014-11-05 18:02:10 -0500

    adding a new class

---
+++ ZZ_testGITProj/src/com/test/pack/testClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.test.pack;
+
+/**
+ * @author sarmat
+ *
+ */
+public class testClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("===>testClass : main :AAAAAA:");
+
+	}
+
+}
