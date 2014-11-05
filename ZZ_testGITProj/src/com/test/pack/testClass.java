31e82050fdc01eb851f6866adfcbc982ff896429
sarmatr <trs_madhu@yahoo.com>
2014-11-05 18:04:05 -0500

    updated TestClass with a extra SOP

--- ZZ_testGITProj/src/com/test/pack/testClass.java
+++ ZZ_testGITProj/src/com/test/pack/testClass.java
@@ -14,6 +14,7 @@ public class testClass {
 	 */
 	public static void main(String[] args) {
 		System.out.println("===>testClass : main :AAAAAA:");
+		System.out.println("===>testClass : main :BBBBBB:");
 
 	}
 
