c9366fec7e0cd7a4701513b8b55982fbcf319137
sarmat <sarmat@192.168.2.2>
2014-11-05 16:04:40 -0500

    intial check in

---
+++ ZZ_testGITProj/src/com/test/pack/myClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.test.pack;
+
+/**
+ * @author sarmat
+ *
+ */
+public class myClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("===>myClass : main :ooookay:");
+
+	}
+
+}
