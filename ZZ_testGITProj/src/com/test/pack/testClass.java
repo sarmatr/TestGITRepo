1b239c8cce9aa99783be2f2c1b532e59f49e1e2f
sarmat <sarmat@192.168.2.2>
2014-11-05 20:20:19 -0500

    Cleanup

--- ZZ_testGITProj/src/com/test/pack/testClass.java
+++ ZZ_testGITProj/src/com/test/pack/testClass.java
@@ -14,11 +14,6 @@ public class testClass {
 	 */
 	public static void main(String[] args) {
 		System.out.println("===>testClass : main :AAAAAA:");
-		System.out.println("===>testClass : main :BBBBBB2222333eeeeeeeeee:");
-		System.out.println("===>testClass : main :BBBBBBRRRRRRRYYYYYzzzzzz:");
-		System.out.println("===>testClass : main :$$$$$$$$$$$$$$$$$$$");
-		System.out.println("===>testClass : main :BBBBBBRRRRRRR:");
-		System.out.println("===>testClass : main :EEEEERRRRRRRRRRR:");
 	}
 
 }
